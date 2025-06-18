7a520d09edfd30c8711f4323054f266e1c630e9d
szwebdesign <info@szwebdesign.ch>
2025-06-18 21:17:16 +0200

    Initial commit - Minesweeper-Projekt

---
+++ Minesweeper - V1/MINESWEEPER/Minesweeper_AKTUELL_3.0/Minesweeper_AKTUELL_3.0/Minesweeper_Logik/Minesweeper_Projektwoche/Minesweeper_Projektwoche/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿// Form1.Designer.cs
+namespace Minesweeper_Projektwoche
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel PanelPlayfield;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.PanelPlayfield = new System.Windows.Forms.Panel();
+            this.PanelScoreTop = new System.Windows.Forms.Panel();
+            this.LblGeschlossenFelder1 = new System.Windows.Forms.Label();
+            this.LblGeschlosseneFelder = new System.Windows.Forms.Label();
+            this.LblOffeneFelder1 = new System.Windows.Forms.Label();
+            this.LblShowScore = new System.Windows.Forms.Label();
+            this.LblOffeneFelder = new System.Windows.Forms.Label();
+            this.LblLEBEN = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnHARD = new System.Windows.Forms.Button();
+            this.BtnMEDIUM = new System.Windows.Forms.Button();
+            this.BtnEASY = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.LblTimerShow = new System.Windows.Forms.Label();
+            this.LblAnzahlBombenSHOW = new System.Windows.Forms.Label();
+            this.LblTimer = new System.Windows.Forms.Label();
+            this.LblAnzahlBomben = new System.Windows.Forms.Label();
+            this.PanelScoreTop.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(172, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Neues Spiel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // PanelPlayfield
+            // 
+            this.PanelPlayfield.Location = new System.Drawing.Point(12, 168);
+            this.PanelPlayfield.Name = "PanelPlayfield";
+            this.PanelPlayfield.Size = new System.Drawing.Size(434, 442);
+            this.PanelPlayfield.TabIndex = 1;
+            // 
+            // PanelScoreTop
+            // 
+            this.PanelScoreTop.Controls.Add(this.LblGeschlossenFelder1);
+            this.PanelScoreTop.Controls.Add(this.LblGeschlosseneFelder);
+            this.PanelScoreTop.Controls.Add(this.LblOffeneFelder1);
+            this.PanelScoreTop.Controls.Add(this.LblShowScore);
+            this.PanelScoreTop.Controls.Add(this.LblOffeneFelder);
+            this.PanelScoreTop.Controls.Add(this.LblLEBEN);
+            this.PanelScoreTop.Location = new System.Drawing.Point(12, 616);
+            this.PanelScoreTop.Name = "PanelScoreTop";
+            this.PanelScoreTop.Size = new System.Drawing.Size(434, 73);
+            this.PanelScoreTop.TabIndex = 2;
+            // 
+            // LblGeschlossenFelder1
+            // 
+            this.LblGeschlossenFelder1.BackColor = System.Drawing.Color.Black;
+            this.LblGeschlossenFelder1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.LblGeschlossenFelder1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblGeschlossenFelder1.ForeColor = System.Drawing.Color.Yellow;
+            this.LblGeschlossenFelder1.Location = new System.Drawing.Point(326, 13);
+            this.LblGeschlossenFelder1.Name = "LblGeschlossenFelder1";
+            this.LblGeschlossenFelder1.Size = new System.Drawing.Size(60, 40);
+            this.LblGeschlossenFelder1.TabIndex = 6;
+            this.LblGeschlossenFelder1.Text = "0";
+            this.LblGeschlossenFelder1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LblGeschlosseneFelder
+            // 
+            this.LblGeschlosseneFelder.AutoSize = true;
+            this.LblGeschlosseneFelder.Font = new System.Drawing.Font("Segoe UI Symbol", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblGeschlosseneFelder.ForeColor = System.Drawing.Color.DarkRed;
+            this.LblGeschlosseneFelder.Location = new System.Drawing.Point(271, 11);
+            this.LblGeschlosseneFelder.Name = "LblGeschlosseneFelder";
+            this.LblGeschlosseneFelder.Size = new System.Drawing.Size(49, 41);
+            this.LblGeschlosseneFelder.TabIndex = 5;
+            this.LblGeschlosseneFelder.Text = "🔒";
+            // 
+            // LblOffeneFelder1
+            // 
+            this.LblOffeneFelder1.BackColor = System.Drawing.Color.Black;
+            this.LblOffeneFelder1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.LblOffeneFelder1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblOffeneFelder1.ForeColor = System.Drawing.Color.Yellow;
+            this.LblOffeneFelder1.Location = new System.Drawing.Point(204, 11);
+            this.LblOffeneFelder1.Name = "LblOffeneFelder1";
+            this.LblOffeneFelder1.Size = new System.Drawing.Size(60, 40);
+            this.LblOffeneFelder1.TabIndex = 4;
+            this.LblOffeneFelder1.Text = "0";
+            this.LblOffeneFelder1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LblShowScore
+            // 
+            this.LblShowScore.BackColor = System.Drawing.Color.Black;
+            this.LblShowScore.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.LblShowScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblShowScore.ForeColor = System.Drawing.Color.Yellow;
+            this.LblShowScore.Location = new System.Drawing.Point(69, 12);
+            this.LblShowScore.Name = "LblShowScore";
+            this.LblShowScore.Size = new System.Drawing.Size(60, 40);
+            this.LblShowScore.TabIndex = 2;
+            this.LblShowScore.Text = "0";
+            this.LblShowScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LblOffeneFelder
+            // 
+            this.LblOffeneFelder.AutoSize = true;
+            this.LblOffeneFelder.Font = new System.Drawing.Font("Segoe UI Symbol", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblOffeneFelder.ForeColor = System.Drawing.Color.LimeGreen;
+            this.LblOffeneFelder.Location = new System.Drawing.Point(149, 13);
+            this.LblOffeneFelder.Name = "LblOffeneFelder";
+            this.LblOffeneFelder.Size = new System.Drawing.Size(49, 41);
+            this.LblOffeneFelder.TabIndex = 1;
+            this.LblOffeneFelder.Text = "🔓";
+            this.LblOffeneFelder.Click += new System.EventHandler(this.LblTOP_Click);
+            // 
+            // LblLEBEN
+            // 
+            this.LblLEBEN.AutoSize = true;
+            this.LblLEBEN.Font = new System.Drawing.Font("Segoe UI Symbol", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblLEBEN.ForeColor = System.Drawing.Color.Red;
+            this.LblLEBEN.Location = new System.Drawing.Point(15, 12);
+            this.LblLEBEN.Name = "LblLEBEN";
+            this.LblLEBEN.Size = new System.Drawing.Size(48, 41);
+            this.LblLEBEN.TabIndex = 0;
+            this.LblLEBEN.Text = "❤️";
+            this.LblLEBEN.Click += new System.EventHandler(this.SCORE_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.BtnHARD);
+            this.panel1.Controls.Add(this.BtnMEDIUM);
+            this.panel1.Controls.Add(this.BtnEASY);
+            this.panel1.Location = new System.Drawing.Point(12, 711);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(429, 73);
+            this.panel1.TabIndex = 4;
+            // 
+            // BtnHARD
+            // 
+            this.BtnHARD.BackColor = System.Drawing.Color.Red;
+            this.BtnHARD.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnHARD.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnHARD.ForeColor = System.Drawing.Color.Black;
+            this.BtnHARD.Location = new System.Drawing.Point(305, 14);
+            this.BtnHARD.Name = "BtnHARD";
+            this.BtnHARD.Size = new System.Drawing.Size(104, 45);
+            this.BtnHARD.TabIndex = 2;
+            this.BtnHARD.Text = "HARD";
+            this.BtnHARD.UseVisualStyleBackColor = false;
+            this.BtnHARD.Click += new System.EventHandler(this.BtnHARD_Click);
+            // 
+            // BtnMEDIUM
+            // 
+            this.BtnMEDIUM.BackColor = System.Drawing.Color.Orange;
+            this.BtnMEDIUM.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnMEDIUM.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnMEDIUM.ForeColor = System.Drawing.Color.Black;
+            this.BtnMEDIUM.Location = new System.Drawing.Point(160, 14);
+            this.BtnMEDIUM.Name = "BtnMEDIUM";
+            this.BtnMEDIUM.Size = new System.Drawing.Size(104, 45);
+            this.BtnMEDIUM.TabIndex = 1;
+            this.BtnMEDIUM.Text = "MEDIUM";
+            this.BtnMEDIUM.UseVisualStyleBackColor = false;
+            this.BtnMEDIUM.Click += new System.EventHandler(this.BtnMEDIUM_Click);
+            // 
+            // BtnEASY
+            // 
+            this.BtnEASY.BackColor = System.Drawing.Color.Lime;
+            this.BtnEASY.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnEASY.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnEASY.ForeColor = System.Drawing.Color.Black;
+            this.BtnEASY.Location = new System.Drawing.Point(16, 13);
+            this.BtnEASY.Name = "BtnEASY";
+            this.BtnEASY.Size = new System.Drawing.Size(104, 45);
+            this.BtnEASY.TabIndex = 0;
+            this.BtnEASY.Text = "EASY";
+            this.BtnEASY.UseVisualStyleBackColor = false;
+            this.BtnEASY.Click += new System.EventHandler(this.BtnEASY_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.LblTimerShow);
+            this.panel2.Controls.Add(this.LblAnzahlBombenSHOW);
+            this.panel2.Controls.Add(this.LblTimer);
+            this.panel2.Controls.Add(this.LblAnzahlBomben);
+            this.panel2.Location = new System.Drawing.Point(12, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(434, 73);
+            this.panel2.TabIndex = 5;
+            // 
+            // LblTimerShow
+            // 
+            this.LblTimerShow.AutoSize = true;
+            this.LblTimerShow.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTimerShow.Location = new System.Drawing.Point(301, 14);
+            this.LblTimerShow.Name = "LblTimerShow";
+            this.LblTimerShow.Size = new System.Drawing.Size(42, 38);
+            this.LblTimerShow.TabIndex = 6;
+            this.LblTimerShow.Text = "⏱️";
+            this.LblTimerShow.Click += new System.EventHandler(this.LblTimerShow_Click);
+            // 
+            // LblAnzahlBombenSHOW
+            // 
+            this.LblAnzahlBombenSHOW.AutoSize = true;
+            this.LblAnzahlBombenSHOW.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblAnzahlBombenSHOW.Location = new System.Drawing.Point(9, 14);
+            this.LblAnzahlBombenSHOW.Name = "LblAnzahlBombenSHOW";
+            this.LblAnzahlBombenSHOW.Size = new System.Drawing.Size(45, 38);
+            this.LblAnzahlBombenSHOW.TabIndex = 5;
+            this.LblAnzahlBombenSHOW.Text = "💣";
+            // 
+            // LblTimer
+            // 
+            this.LblTimer.BackColor = System.Drawing.Color.Black;
+            this.LblTimer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.LblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTimer.ForeColor = System.Drawing.Color.Red;
+            this.LblTimer.Location = new System.Drawing.Point(349, 12);
+            this.LblTimer.Name = "LblTimer";
+            this.LblTimer.Size = new System.Drawing.Size(60, 40);
+            this.LblTimer.TabIndex = 4;
+            this.LblTimer.Text = "0";
+            this.LblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LblAnzahlBomben
+            // 
+            this.LblAnzahlBomben.BackColor = System.Drawing.Color.Black;
+            this.LblAnzahlBomben.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.LblAnzahlBomben.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblAnzahlBomben.ForeColor = System.Drawing.Color.Red;
+            this.LblAnzahlBomben.Location = new System.Drawing.Point(60, 12);
+            this.LblAnzahlBomben.Name = "LblAnzahlBomben";
+            this.LblAnzahlBomben.Size = new System.Drawing.Size(60, 40);
+            this.LblAnzahlBomben.TabIndex = 2;
+            this.LblAnzahlBomben.Text = "0";
+            this.LblAnzahlBomben.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LblAnzahlBomben.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(458, 796);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.PanelScoreTop);
+            this.Controls.Add(this.PanelPlayfield);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Minesweeper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.PanelScoreTop.ResumeLayout(false);
+            this.PanelScoreTop.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.Panel PanelScoreTop;
+        private System.Windows.Forms.Label LblLEBEN;
+        private System.Windows.Forms.Label LblOffeneFelder;
+        private System.Windows.Forms.Label LblShowScore;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BtnHARD;
+        private System.Windows.Forms.Button BtnMEDIUM;
+        private System.Windows.Forms.Button BtnEASY;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label LblAnzahlBomben;
+        private System.Windows.Forms.Label LblTimer;
+        private System.Windows.Forms.Label LblOffeneFelder1;
+        private System.Windows.Forms.Label LblGeschlossenFelder1;
+        private System.Windows.Forms.Label LblGeschlosseneFelder;
+        private System.Windows.Forms.Label LblAnzahlBombenSHOW;
+        private System.Windows.Forms.Label LblTimerShow;
+    }
+}
---
+++ Minesweeper - V1/MINESWEEPER/Minesweeper_AKTUELL_3.0/Minesweeper_AKTUELL_3.0/Minesweeper_Logik/Minesweeper_Projektwoche/Minesweeper_Projektwoche/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Minesweeper_Projektwoche
+{
+    public partial class Form1 : Form
+    {
+        private Button[,] spielfeld = new Button[8, 8];
+        private bool[,] bomben = new bool[8, 8];
+        private int geöffneteFelder = 0;
+        private int anzahlBomben = 8;   // Standardwert: leicht
+        private int startLeben = 5;     // Standardwert: leicht
+        private Random rand = new Random();
+
+        private Timer timer = new Timer();
+        private int sekunden = 0;
+
+        private int leben = 5; // Startleben
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Interval = 1000; // 1 Sekunde
+            timer.Tick += Timer_Tick;
+            ErstelleSpielfeld();
+            AktualisiereFeldAnzahl();
+            AktualisiereLeben();
+            AktualisiereBombenAnzahl();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            sekunden++;
+            LblTimer.Text = sekunden.ToString();
+        }
+
+        private void ErstelleSpielfeld()
+        {
+            PanelPlayfield.Controls.Clear();
+            geöffneteFelder = 0;
+            sekunden = 0;
+            LblTimer.Text = "0";
+            leben = startLeben;
+            AktualisiereLeben();
+
+            bomben = new bool[8, 8];
+            spielfeld = new Button[8, 8];
+
+            List<(int, int)> alleFelder = new List<(int, int)>();
+            for (int x = 0; x < 8; x++)
+                for (int y = 0; y < 8; y++)
+                    alleFelder.Add((x, y));
+
+            for (int i = 0; i < anzahlBomben; i++) // 8 Bomben
+            {
+                int index = rand.Next(alleFelder.Count);
+                var (bx, by) = alleFelder[index];
+                bomben[bx, by] = true;
+                alleFelder.RemoveAt(index);
+            }
+
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    Button button = new Button();
+                    button.Size = new Size(40, 40);
+                    button.Location = new Point(x * 40, y * 40);
+                    button.MouseUp += ButtonMouseUp;
+                    button.Tag = new Point(x, y);
+                    button.TabStop = false;
+                    PanelPlayfield.Controls.Add(button);
+                    spielfeld[x, y] = button;
+                    button.Enabled = true;
+                    button.Text = "";
+                    button.BackColor = default(Color);
+                }
+            }
+
+            AktualisiereBombenAnzahl();
+
+            timer.Start();
+        }
+
+        private void DeckeFreieFelderAuf(int x, int y)
+        {
+            // Wenn das Feld außerhalb des Bereichs ist → abbrechen
+
+            if (x < 0 || x >= 8 || y < 0 || y >= 8)
+                return;
+
+            Button btn = spielfeld[x, y];
+
+            // Wenn das Feld schon deaktiviert ist oder eine Flagge hat → abbrechen
+
+            if (!btn.Enabled || btn.Text == "🚩")
+                return;
+
+            int nachbarBomben = ZaehleNachbarBomben(x, y);
+            btn.BackColor = Color.LightGray;
+            btn.Text = nachbarBomben > 0 ? nachbarBomben.ToString() : "";
+            btn.Enabled = false;
+            geöffneteFelder++;
+            AktualisiereFeldAnzahl();
+
+            // Wenn keine Nachbarbomben → alle Nachbarn auch aufdecken
+
+            if (nachbarBomben == 0)
+            {
+                for (int dx = -1; dx <= 1; dx++)
+                {
+                    for (int dy = -1; dy <= 1; dy++)
+                    {
+                        if (dx != 0 || dy != 0) // nicht sich selbst
+
+                            DeckeFreieFelderAuf(x + dx, y + dy);
+                    }
+                }
+            }
+        }
+
+        private void SetzeSchwierigkeitsgrad(string schwierigkeitsgrad)
+        {
+            switch (schwierigkeitsgrad)
+            {
+                case "Leicht":
+                    anzahlBomben = 8;
+                    startLeben = 5;
+                    break;
+                case "Mittel":
+                    anzahlBomben = 16;
+                    startLeben = 3;
+                    break;
+                case "Schwer":
+                    anzahlBomben = 24;
+                    startLeben = 1;
+                    break;
+            }
+
+            leben = startLeben;                 // Leben setzen
+            ErstelleSpielfeld();               // neues Spielfeld starten
+            AktualisiereFeldAnzahl();          // Anzeige aktualisieren
+        }
+
+
+        private void AktualisiereBombenAnzahl()
+        {
+            int bombenAnzahl = 0;
+            for (int x = 0; x < 8; x++)
+                for (int y = 0; y < 8; y++)
+                    if (bomben[x, y]) bombenAnzahl++;
+
+            LblAnzahlBomben.Text = bombenAnzahl.ToString();
+        }
+
+        private void ButtonMouseUp(object sender, MouseEventArgs e)
+        {
+            Button btn = (Button)sender;
+            Point pos = (Point)btn.Tag;
+            int x = pos.X;
+            int y = pos.Y;
+
+            if (e.Button == MouseButtons.Right && btn.Enabled)
+            {
+                btn.Text = btn.Text == "🚩" ? "" : "🚩";
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                ButtonKlick(btn);
+            }
+        }
+
+        private void ButtonKlick(Button btn)
+
+
+        {
+
+
+
+            if (!btn.Enabled || btn.Text == "🚩") return;
+
+            Point pos = (Point)btn.Tag;
+            int x = pos.X;
+            int y = pos.Y;
+
+            if (bomben[x, y])
+            {
+                leben--;
+                AktualisiereLeben();
+                btn.BackColor = Color.Red;
+                btn.Text = "💣";
+                btn.Enabled = false;
+
+                if (leben <= 0)
+                {
+                    MessageBox.Show("💥 Bumm! Du hast verloren.");
+                    SpielBeenden();
+                    timer.Stop();
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show($"💥 Mine getroffen! Leben übrig: {leben}");
+                }
+            }
+
+
+            else
+            {
+                DeckeFreieFelderAuf(x, y); // 🆕 Rekursives Aufdecken
+
+                if (geöffneteFelder == (8 * 8 - anzahlBomben)) // Alle Nicht-Bombenfelder offen
+                {
+                    SpielGewonnen();
+                    timer.Stop();
+                }
+            }
+        }
+            
+              
+        
+
+        private int ZaehleNachbarBomben(int x, int y)
+        {
+            int count = 0;
+            for (int dx = -1; dx <= 1; dx++)
+            {
+                for (int dy = -1; dy <= 1; dy++)
+                {
+                    int nx = x + dx;
+                    int ny = y + dy;
+                    if (nx >= 0 && nx < 8 && ny >= 0 && ny < 8 && bomben[nx, ny])
+                        count++;
+                }
+            }
+            return count;
+        }
+
+        private void SpielBeenden()
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    if (bomben[x, y])
+                    {
+                        spielfeld[x, y].BackColor = Color.Red;
+                        spielfeld[x, y].Text = "💣";
+                    }
+                    spielfeld[x, y].Enabled = false;
+                }
+            }
+        }
+
+        private void SpielGewonnen()
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    if (bomben[x, y])
+                    {
+                        spielfeld[x, y].Text = "🚩";
+                        spielfeld[x, y].BackColor = Color.LightGreen;
+                    }
+                    spielfeld[x, y].Enabled = false;
+                }
+            }
+            MessageBox.Show("🎉 Glückwunsch, du hast gewonnen!");
+        }
+
+        private void AktualisiereFeldAnzahl()
+        {
+            int geschlosseneFelder = 0;
+            int offeneFelder = 0;
+
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    if (spielfeld[x, y].Enabled)
+                        geschlosseneFelder++;  // Button ist noch aktiv = geschlossen
+                    else
+                        offeneFelder++;        // Button ist deaktiviert = offen
+                }
+            }
+
+            LblGeschlossenFelder1.Text = geschlosseneFelder.ToString();
+            LblOffeneFelder1.Text = offeneFelder.ToString();
+        }
+
+        private void AktualisiereLeben()
+        {
+            LblShowScore.Text = leben.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Optional: neues Spiel automatisch starten beim Laden
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            ErstelleSpielfeld();
+            AktualisiereFeldAnzahl();
+        }
+
+        // Dummy Events aus Designer, können leer bleiben
+        private void SCORE_Click(object sender, EventArgs e) { }
+        private void label3_Click(object sender, EventArgs e) { }
+        private void LblTOP_Click(object sender, EventArgs e) { }
+
+        private void BtnEASY_Click(object sender, EventArgs e)
+        {
+            SetzeSchwierigkeitsgrad("Leicht");
+        }
+
+        private void BtnMEDIUM_Click(object sender, EventArgs e)
+        {
+            SetzeSchwierigkeitsgrad("Mittel");
+        }
+
+        private void BtnHARD_Click(object sender, EventArgs e)
+        {
+            SetzeSchwierigkeitsgrad("Schwer");
+        }
+
+        private void LblTimerShow_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
